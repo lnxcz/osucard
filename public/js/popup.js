57f4a5dbd96218b52672c7abb7ee4e1483fdfdd9
lnxcz <in@lnx.rip>
2021-07-24 14:34:48 +0200

    create popup

--- public/js/popup.js
+++ public/js/popup.js
@@ -2,25 +2,40 @@ const button = document.getElementById("create");
 const closeBtn = document.getElementById("closeBtn");
 
 button.addEventListener("click", e => {
-  e.preventDefault();
+  e.preventDefault()
   
-  const color = document.getElementById("color").value;
-  const username = document.getElementById("username").value;
-
-  const url = window.location.href + "card/" + username + "?color=" + color;
-
-  document.getElementById("popup").style = "display: block;";
   document.getElementById("background").style = "display: block;";
 
-  document.getElementById("card").setAttribute("src", url);
-  document.getElementById("link").innerText = url;
-  document.getElementById("nostyle").href = url;
+//   const color = document.getElementById("color").value;
+//   const username = document.getElementById("username").value;
+
+//   const url = window.location.href + "card/" + username + "?color=" + color;
+
+   document.getElementById("popup").style = "display: block;";
+//   document.getElementById("background").style = "display: block;";
+
+//   document.getElementById("card").setAttribute("src", url);
+//   document.getElementById("link").innerText = url;
+//   document.getElementById("nostyle").href = url;
 })
 
 closeBtn.addEventListener("click", e => {
     document.getElementById("popup").style = "display: none;";
   document.getElementById("background").style = "display: none;";
 })
+
+
+function copyText() {
+  var copyText = document.getElementById("link");
+
+  copyText.select();
+  copyText.setSelectionRange(0, 99999);
+
+  document.execCommand("copy");
+
+ 
+  // TODO: Some alert here
+}
 
 
 //this was made by atskukoro

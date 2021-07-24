41f3b609cfb69acb0c1da85ff3c011b9dffcef14
lnxcz <in@lnx.rip>
2021-07-24 03:14:22 +0200

    css

---
+++ public/js/popup.js
@@ -0,0 +1,26 @@
+const button = document.getElementById("create");
+const closeBtn = document.getElementById("closeBtn");
+
+button.addEventListener("click", e => {
+  e.preventDefault();
+  
+  const color = document.getElementById("color").value;
+  const username = document.getElementById("username").value;
+
+  const url = window.location.href + "card/" + username + "?color=" + color;
+
+  document.getElementById("popup").style = "display: block;";
+  document.getElementById("background").style = "display: block;";
+
+  document.getElementById("card").setAttribute("src", url);
+  document.getElementById("link").innerText = url;
+  document.getElementById("nostyle").href = url;
+})
+
+closeBtn.addEventListener("click", e => {
+    document.getElementById("popup").style = "display: none;";
+  document.getElementById("background").style = "display: none;";
+})
+
+
+//this was made by atskukoro
